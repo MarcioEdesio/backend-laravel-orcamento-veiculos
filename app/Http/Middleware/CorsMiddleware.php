cd600bf9dc08f40b4780d739a7bac63b9fcfc371
MarcioEdesio <marcioedesio@gmail.com>
2025-07-02 08:34:57 -0300

    Middleware CORS atualizado para lidar com OPTIONS

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,6 +8,13 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('', 204)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        }
+
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
